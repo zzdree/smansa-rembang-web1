d6fd4ac708c83ec9f5d43f4be3f933901ef8f683
Andreas Restuawanta Christwara <51115151+zzdree@users.noreply.github.com>
2026-09-01 04:01:13 +0700

    chore(sanity): set deployment appId c4n5kzqv after successful deploy

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,4 +2,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api:{ projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'g061zdty', dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production' },
   studioHost: 'smansa-rembang-web1',
+  deployment: { appId: 'c4n5kzqvj6ggqcbrhifqoc41', autoUpdates: true },
 })
+
